8cf42b46965168073a406fe0a54bb0939756165d
chri-s <Chri-s@users.noreply.github.com>
2024-03-31 22:12:32 +0200

    Add zip file creation to Build.bat

--- Build-Setup.bat
+++ Build-Setup.bat
@@ -1,2 +1,5 @@
+del /Q Setup\en-US\*
 dotnet.exe build -c Release-Dependent -p Platform=x64 -o Setup src\Setup
 dotnet.exe build -c Release-Standalone -p Platform=x64 -o Setup src\Setup
+powershell Compress-Archive -Force -CompressionLevel Optimal src\FavoritesMenu\bin\Release\net8.0-windows\Dependent\* Setup\en-US\FavoritesMenu.Dependent.zip
+powershell Compress-Archive -Force -CompressionLevel Optimal src\FavoritesMenu\bin\Release\net8.0-windows\Standalone\* Setup\en-US\FavoritesMenu.Standalone.zip
